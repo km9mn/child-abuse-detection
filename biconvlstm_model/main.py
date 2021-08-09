79d2c9d92ef7d57d933b904e19e9f9bc6658d86b
Seogki Kim <skkim4968@gmail.com>
2021-08-09 14:28:58 +0900

    added biconvlstm_model

---
+++ biconvlstm_model/main.py
@@ -0,0 +1,377 @@
+# Based on code from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+
+import datetime
+import argparse
+import os
+import re
+import random
+import shutil
+import time
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torch.optim
+import torch.utils.data
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+from data.data_reader import DatasetReader
+from data.data_splitter import DatasetSplit
+from data.data_transformer import DatasetTransform
+from data.transforms import SelectFrames, FrameDifference, Downsample, TileVideo, RandomCrop, Resize, RandomHorizontalFlip, Normalize, ToTensor
+
+model_names = ['E', 'E_bi', 'E_bi_avg_pool', 'E_bi_max_pool']
+data_names = ['FD','RWF','AH','UCF','ALL']
+
+
+parser = argparse.ArgumentParser(description='PyTorch Violence Predictor Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('data_name', default='H',
+                    help='data name: ' +
+                    ' | '.join(data_names) +
+                    ' (default: H)')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='E_bi_max_pool',
+                    choices=model_names,
+                    help='model architecture: ' +
+                    ' | '.join(model_names) +
+                    ' (default: E)')
+parser.add_argument('--t', '--transform', dest='transform', default='RF', type=str,
+                    help='type of data transform [R: resize, C: random crop and flip, RC: both] (default: R)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('-b', '--batch-size', default=8, type=int,
+                    metavar='N', help='mini-batch size (default: 1)')
+parser.add_argument('--lr', '--learning-rate', default=1e-6, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--weight-decay', '--wd', default=1e-1, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--c', '--cpu', dest='cpu', action='store_true',
+                    help='evaluate model on cpu')
+parser.add_argument('--k', '--kfold', dest='kfold', default=0, type=int,
+                    help='evaulate model with kfold index as test')
+parser.add_argument('--s', '--split', dest='split', default=5, type=int,
+                    help='fractional split of training/validation data. I.e. 5 -> 1/5 data is validation')
+parser.add_argument('--f', '--frames', dest='frames', default=20, type=int,
+                    help='number of frame diffs per video')
+parser.add_argument('--i', '--id', dest='ID', default='', type=str,
+                    help='id of the learning')
+best_prec = 0
+
+seed = 250
+
+
+def main():
+    global args, best_prec
+    args = parser.parse_args()
+
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    data = DatasetReader(root_dir=args.data, data_name=args.data_name)
+
+    assert args.kfold < args.split, "kfold index must be less than the split size!"
+    folds = fold(args.split, data)
+    for i in range(args.kfold):
+        next(folds)
+
+    (train_dataset, val_dataset) = next(folds)
+
+    def transform_factory(transformation):
+
+        if(transformation == "RC"):
+            train_transform = transforms.Compose([Resize(size=256), RandomCrop(size=224), RandomHorizontalFlip()])
+        elif(transformation == "R"):
+            train_transform = transforms.Compose([Resize(size=224)])
+        elif(transformation == "RF"):
+            train_transform = transforms.Compose([Resize(size=224), RandomHorizontalFlip()])
+
+        return train_transform
+
+    train_transformations = transforms.Compose([transform_factory(args.transform), SelectFrames(num_frames=20), FrameDifference(dim=0), Normalize(), ToTensor()])
+    val_transformations = transforms.Compose([Resize(size=224), SelectFrames(num_frames=20), FrameDifference(dim=0), Normalize(), ToTensor()])
+    print(val_transformations)
+    train_dataset = DatasetTransform(train_dataset, train_transformations)
+    val_dataset = DatasetTransform(val_dataset, val_transformations)
+    print(val_dataset)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    print(val_loader)
+
+    # create model
+    print("=> creating model '{}'".format(args.arch))
+    VP = network_factory(args.arch)
+
+    model = VP()
+
+    if not args.cpu:
+        model = model.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), args.lr,
+                                 weight_decay=args.weight_decay)
+
+    loss = list()
+    acc = list()
+    prec = list()
+    path = '/content/gdrive/Shareddrives/2021청년인재_고려대과정_10조/BiConvLSTM_Violence_Detection_Spatiotemporal_Encoder/acc_graph/'
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            f_name = os.path.basename(args.resume)
+            graph = np.load(path+'acc_loss_prec_' + re.findall(r'_t(.+).tar',fname)[0])
+            loss = graph[0]
+            acc = graph[1]
+            prec = graph[2]
+
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            print('start_epoch : ', args.start_epoch)
+            best_prec = checkpoint['best_prec']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    if args.evaluate:
+        validate(val_loader, model)
+        return
+    now = datetime.datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    run_training(train_loader, val_loader, model, criterion, optimizer, best_prec, nowDatetime, acc,loss, prec, path, args.ID)
+
+
+def run_training(train_loader, val_loader, model, criterion, optimizer, best_prec, starttime, accuray_list,loss_list, precision_list, path, ID):
+
+    epoch = 0
+
+    while True:
+        # train for one epoch
+        acc, loss = train(train_loader, model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        prec = validate(val_loader, model)
+        
+        # acc, loss, prec 저장
+        accuray_list.append(acc)
+        loss_list.append(loss)
+        precision_list.append(prec)
+
+        # graph 및 acc,loss,prec 저장
+        save_accuracy_graph(path, accuray_list, loss_list, precision_list, starttime)
+        np.save(path + 'acc_loss_prec_' + starttime ,[accuray_list,loss_list,precision_list])
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec > best_prec
+        best_prec = max(prec, best_prec)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_prec': best_prec,
+            'optimizer': optimizer.state_dict(),
+            }, is_best, args.arch + "_" + args.data_name + "_fold_" + str(args.kfold),id=ID, starttime=starttime)
+        
+        epoch += 1
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    prec = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if not args.cpu:
+            target = target.cuda(non_blocking=True)
+
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+        if not args.cpu:
+            input_var = input_var.cuda()
+            target_var = target_var.cuda()
+
+        # compute output
+        output_dict = model(input_var)
+        loss = criterion(output_dict['classification'], target_var)
+
+        # measure accuracy and record loss
+        precision = accuracy(output_dict['classification'], target)
+        losses.update(loss.data, input.size(0))
+        prec.update(precision, input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    print('Epoch: [{0}][{1}/{2}]\t'
+          'Time {batch_time.avg:.3f}\t'
+          'Data {data_time.avg:.3f}\t'
+          'Loss {loss.avg:.4f}\t'
+          'Accuracy {prec.avg:.4f}'.format(
+              epoch + 1, i + 1, len(train_loader), batch_time=batch_time,
+              data_time=data_time, loss=losses, prec=prec))
+    return prec.avg, losses.avg
+
+def validate(val_loader, model):
+    batch_time = AverageMeter()
+    prec = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+
+        if not args.cpu:
+            target = target.cuda(non_blocking=True)
+
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+        if not args.cpu:
+            input_var = input_var.cuda()
+            target_var = target_var.cuda()
+
+        # compute output
+        output_dict = model(input_var)
+
+        # measure accuracy and record loss
+        #print('target : ',target)
+        #print('output : ',output_dict['classification'])    
+        precision = accuracy(output_dict['classification'], target)
+        prec.update(precision, input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    print('Test: [{0}/{1}]\t'
+          'Time {batch_time.avg:.3f}\t'
+          'Accuracy {prec.avg:.4f}'.format(
+              i + 1, len(val_loader), batch_time=batch_time, prec=prec))
+
+    return prec.avg
+
+
+def save_checkpoint(state, is_best, id='someid', starttime='tmp'):
+    filename = 'checkpoint.' + str(id) +'_t'+ starttime + '.tar'
+    torch.save(state, filename)
+    if is_best:
+        model_best_filename = 'model_best.' + str(id) + '_t' +starttime+'.tar'
+        shutil.copyfile(filename, model_best_filename)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target):
+    """Computes the precision of model"""
+    batch_size = target.size(0)
+
+    _, pred = torch.max(output.data, 1)
+    correct = pred.eq(target).cpu().sum()
+
+    res = correct * 100.0 / batch_size
+    return res
+
+# DatasetSplit (data, index, length)
+def fold(folds, data): #folds : 몇개로 나눌건가 , data : DatasetReader
+    tot_length = len(data) #tot_length: 영상 총 개수
+    split_length = tot_length // folds # // : 나누기 결과에서 소수점 버려줌, split_length : 몇개가 한묶음?
+    for i in range(folds): # folds =3, split_length = 5, tot_length = 15일때 i= 0,1,2
+        train_dataset = DatasetSplit(data, (i + 1) * split_length, tot_length - split_length) # 5, 10개/ 10, 10개 / 15 , 10개
+        val_dataset = DatasetSplit(data, i * split_length, split_length) # 0,5개 / 5 , 5개/ 10 , 5개
+        yield (train_dataset, val_dataset)
+# 총 영상 개수 15개 일때 3 folds를 하면 5개는 val/ 10개는 train
+# idx = 3
+# train_dataset -> self.index = 5, length = 10 -> index = (5+6) % 10 = 1
+# val_dataset -> self.index = 0, length = 5
+
+
+def network_factory(arch):
+
+    if arch == 'E':
+        from networks.E import VP
+
+    elif arch == 'E_bi':
+        from networks.E_bi import VP
+
+    elif arch == 'E_bi_avg_pool':
+        from networks.E_bi_avg_pool import VP
+
+    elif arch == 'E_bi_max_pool':
+        from networks.E_bi_max_pool import VP
+
+    else:
+        assert 0, "Bad network arch name: " + arch
+
+    return VP
+
+def save_accuracy_graph(path, accuracy,loss, prec, starttime, filename=''): 
+    # 이때 filename으로 파일이 저장되고 이때 시점에 accuracy(list) 등에 저장된 것으로 시각화 
+    plt.figure(figsize=(10,10))
+
+    plt.subplot(2,1,1)
+    plt.title('model accuracy')
+    plt.plot(accuracy, color='b', label='accuracy')
+    plt.plot(loss, color='r', label='loss')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['accuracy', 'loss'], loc='upper left')
+
+    plt.subplot(2,1,2)
+    plt.plot(prec, color='g', label='precision')
+    plt.title('test accuracy')
+    plt.ylabel('precision')
+    plt.xlabel('epoch')
+    plt.legend(['precision'], loc='upper left')
+    plt.savefig(path+filename+starttime+'_acc_loss.jpg')
+    #plt.show()
+  
+if __name__ == '__main__':
+    main()
